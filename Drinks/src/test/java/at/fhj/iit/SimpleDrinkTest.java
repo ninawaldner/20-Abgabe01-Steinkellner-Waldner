07e8676a4850bc1ad9f4f0be1e2fdd7434f8bdf0
ninawaldner <nina.waldner@edu.campus02.at>
2020-05-07 11:47:04 +0200

    started test in SimpleDinkTest file

--- Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -1,5 +1,51 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testing Liquid implementation")
 class SimpleDrinkTest {
 
+    /**
+     *
+     */
+   protected Liquid l;
+    private SimpleDrink wine;
+
+    /**
+     * inits an alcoholic liquid for EACH test
+     */
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        l = new Liquid("Wein", 0.125, 13);
+       wine = new SimpleDrink("Wein", l);
+    }
+
+    @Test
+    @DisplayName("Testing constructor non alcoholic")
+    public void testConstructorNonAlcoholic(){
+        assertEquals(wine.l.getName(), "Wein");
+        assertEquals(wine.l.getAlcoholPercent(),   13);
+        assertEquals(wine.l.getVolume(), 0.125);
+    }
+
+    @Test
+    @DisplayName("Testing constructor non alcoholic")
+    void getVolume() {
+
+    }
+
+    @Test
+    @DisplayName("Testing volume setter")
+    void getAlcoholPercent() {
+    }
+
+    @Test
+    @DisplayName("Testing volume setter")
+    void isAlcoholic() {
+    }
 }
